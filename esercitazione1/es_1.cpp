8bc8c72a92d597dd3936444d19758413f71557dc
Noemi Anzaldi <s325034@studenti.polito.it>
2026-03-14 14:54:18 +0100

    ho caricato la versione finale dell'esercizio

--- esercitazione1/es_1.cpp
+++ esercitazione1/es_1.cpp
@@ -1,21 +1,30 @@
 #include <fstream>
-#include <iosream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(void) {
-	cout << "Inserisci il nome del file: \n";
-	string nome_file;
-	cin >> nome_file;
-	ifstream ifs(nome_file);
-	if ( ifs.is_open() {
-		while( !ifs.eof() ) {
-			string luogo;
-			double t1, t2, t3, t4;
-			while (ifs >> luogo >> t1 >> t2 >> t3 >> t4) {
-				double media = (t1 + t2 + t3 +t4) / 4.0;
-				cout << luogo << media << "\n";
+int main(int argc, const char *argv[]) {
+	if (argc >= 2){ 
+		string name_file = argv[1]; // il nome del file è scritto come secondo paramentro dall'utente
+		ifstream file_input(name_file); // apro il file in modalità lettura
+		if (file_input.is_open()){ // verifico se il file è stato aperto correttamente
+			string città;
+			double t1, t2, t3, t4; 
+			for (int i=0; i<3; i++){ 
+				if (file_input >> città >> t1 >> t2 >> t3 >> t4){
+				double media = (t1+t2+t3+t4)/4.0; //calcolo la media dei 4 numeri letti per ogni città
+				cout << città << " " << media << "\n"; //stampo il nome della città con la sua
+				}
 			}
 		}
+		else {
+			cerr << "Errore: Il nome del file non esiste o non può essere aperto correttamente. \n";
+			return 1;
+		}
+	}
+	else {
+		cerr << "Errore: Il file non è stato dichiarato come paramentro. \n";
+		return 1;
 	}
 	return 0;
-}
+}	
